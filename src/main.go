02a3b43fabf63ec85eb9229b19e1ef800e8e16ca
Neutroflash <neutroflashp@hotmail.com>
2024-09-05 09:30:58 -0500

    declarando variables

--- src/main.go
+++ src/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hola Mundo!")
+	//Declaración de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	fmt.Println("pi: ", pi)
+	fmt.Println("pi2: ", pi2)
+
+	//Declaración de variables enteras
+	base := 12
+	var altura int = 14
+	var area int 
+
+	fmt.Println(base, altura, area)
+
+	//Zero Values
+	var a int 
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a,b,c,d)
+
+	//Area Cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := math.Pow(baseCuadrado, 2)
+	fmt.Println(areaCuadrado)
 }
