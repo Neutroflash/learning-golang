84d871ac17cc1fb80661d6812589e42647314ed7
Neutroflash <neutroflashp@hotmail.com>
2024-09-11 14:03:34 -0500

    slices and arrays

--- src/main.go
+++ src/main.go
@@ -247,4 +247,29 @@ func main() {
 			break
 		}
 	}
+
+	//Array
+	var array [4]int 
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
+
+	//Slices
+	slice := []int{0,1,2,3,4,5,6}
+	fmt.Println(slice, len(slice), cap(slice))
+
+	//Métodos en Slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	//Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	//Append nueva list
+	newSLice := []int{8,9,10}
+	slice = append(slice, newSLice...)
+	fmt.Println(slice)
 }
