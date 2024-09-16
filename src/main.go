d1ea83a0b3771002aa4da73a327b6081ebe4e19e
Neutroflash <neutroflashp@hotmail.com>
2024-09-16 10:58:16 -0500

    canales-concurrencia

--- src/main.go
+++ src/main.go
@@ -110,7 +110,50 @@ func say(text string, wg *sync.WaitGroup) {
 	fmt.Println(text)
 }
 
+func say2(text string, canal chan<- string) {
+	canal <- text
+}
+
+func message(text string, c chan string) {
+	c <- text
+}
+
 func main() {
+	canal2 := make(chan string, 2)
+	canal2 <- "Mensaje 1"
+	canal2 <- "Mensaje 2"
+
+	fmt.Println(len(canal2), cap(canal2))
+
+	//Range y Close
+	close(canal2)
+
+	//canal2 <- "Mensaje 3"
+	for message := range canal2 {
+		fmt.Println(message)
+	}
+
+	//select
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("mensaje1", email1)
+	go message("mensaje2", email2)
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email1", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibidio de email2", m2)
+		}
+	}
+
+	canal := make(chan string, 1)
+	fmt.Println("Hello")
+
+	go say2("Bye", canal)
+	fmt.Println(<-canal)
+
 	var wg sync.WaitGroup
 
 	fmt.Println("Hello")
@@ -118,8 +161,8 @@ func main() {
 	go say("World", &wg)
 	wg.Wait()
 
-	go func (text string)  {
-		fmt.Println(text)	
+	go func(text string) {
+		fmt.Println(text)
 	}("Adios")
 	time.Sleep(time.Second * 1)
 
